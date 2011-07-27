5ed8145030fb1da6737cb3f030072b9adee88409
Joshua Introne <jintrone@mit.edu>
2011-07-27 16:38:24 -0400

    New file for passing form params back to spring

---
+++ src/main/java/edu/mit/cci/turksnet/web/NodeForm.java
@@ -0,0 +1,42 @@
+package edu.mit.cci.turksnet.web;
+
+/**
+ * User: jintrone
+ * Date: 7/26/11
+ * Time: 9:16 PM
+ */
+public class NodeForm {
+
+    public String privateData;
+    public String publicData;
+     public String assignmentId;
+
+    public String getPrivateData() {
+        return privateData;
+    }
+
+    public void setPrivateData(String privateData) {
+        this.privateData = privateData;
+    }
+
+    public String getPublicData() {
+        return publicData;
+    }
+
+    public void setPublicData(String publicData) {
+        this.publicData = publicData;
+    }
+
+    public String getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(String assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+
+
+
+
+}
